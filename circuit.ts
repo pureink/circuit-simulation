82e3af9d092f083acf1ec1c4da6393b734e7ed6b
root <root@DESKTOP-EK50T47.localdomain>
2020-09-07 13:52:22 +0800

    add some function

--- circuit.ts
+++ circuit.ts
@@ -1,5 +1,27 @@
-function test(){
+//从前端中获取json数据
+function getJson(){
     const temp = (<HTMLInputElement>document.getElementById("data")).value;
-    const testjson=JSON.parse(temp);
-    alert(testjson.name)
- }
+    return JSON.parse(temp);
+    
+}
+function main(){
+    //const json=getJson()
+    const jsontest='{"name":"hello"}'
+    const json=JSON.parse(jsontest)
+    alert(json.name)
+}
+//传入base64字符串转为utf8函数字符串
+ function b64_to_utf8( str:string ) {
+    return decodeURIComponent(escape(window.atob( str )));
+}
+//示例base64编码函数字符串
+const teststr="aXRlbXNbMl09aXRlbXNbMV0raXRlbXNbMF0NCnJldHVybiBpdGVtcw=="
+console.log('测试编码转化'+b64_to_utf8(teststr))
+let items=[1,1,1]
+//利用Function函数将字符串转换为运行时函数
+function looseJsonParse(obj:string){
+    return Function('"use strict";'+obj)();
+}
+console.log(items)
+looseJsonParse(b64_to_utf8(teststr))
+console.log(items)
--- js/circuit.js
+++ js/circuit.js
@@ -1,6 +1,26 @@
 "use strict";
-function test() {
+//从前端中获取json数据
+function getJson() {
     var temp = document.getElementById("data").value;
-    var testjson = JSON.parse(temp);
-    alert(testjson.name);
+    return JSON.parse(temp);
 }
+function main() {
+    //const json=getJson()
+    var jsontest = '{"name":"hello"}';
+    var json = JSON.parse(jsontest);
+    alert(json.name);
+}
+//传入base64字符串转为utf8函数字符串
+function b64_to_utf8(str) {
+    return decodeURIComponent(escape(window.atob(str)));
+}
+//示例base64编码函数字符串
+var teststr = "aXRlbXNbMl09aXRlbXNbMV0raXRlbXNbMF0NCnJldHVybiBpdGVtcw==";
+console.log('测试编码转化' + b64_to_utf8(teststr));
+var items = [1, 1, 1];
+function looseJsonParse(obj) {
+    return Function('"use strict";' + obj)();
+}
+console.log(items);
+looseJsonParse(b64_to_utf8(teststr));
+console.log(items);
